9c50ac63e5d2c64fae782e20b797870c4b8462fe
kwonjihyeon2 <kjh2868@gmail.com>
2022-03-24 11:19:44 +0900

    Feat: 디자인 수정본 적용

--- src/components/units/market/customCenter/CustomerCenter.presenter.tsx
+++ src/components/units/market/customCenter/CustomerCenter.presenter.tsx
@@ -1,59 +1,105 @@
-import { useState } from "react";
-import * as S from "./CustomerBoard.styled";
-import NoticeItem from "./CustomerNotice";
+import { useState } from 'react'
+import * as S from './CustomerBoard.styled'
+import NoticeItem from './CustomerNotice'
 
 export default function CustomCenterPageUI() {
   const noticeArr = [
-    { index: 1, title: "교환/환불 규정 안내", date: "2022-03-03", 조회: 15 },
+    { index: 1, title: '교환/환불 규정 안내', date: '2022-03-03', 조회: 15 },
     {
       index: 2,
-      title: "검수 기준",
-      date: "2022-03-03",
+      title: '검수 기준',
+      date: '2022-03-03',
       조회: 110,
     },
-    { index: 3, title: "SOS NEW OPEN", date: "2022-03-03", 조회: 23 },
-  ];
+    { index: 3, title: 'SOS NEW OPEN', date: '2022-03-03', 조회: 23 },
+  ]
+  const QnaArr = [
+    {
+      index: 1,
+      title: '배송비는 항상 무료인가요?',
+      date: '2022-03-03',
+      조회: 15,
+    },
+    {
+      index: 2,
+      title: '100% 진품 확실한가요?',
+      date: '2022-03-03',
+      조회: 110,
+    },
+    { index: 3, title: '교환, 환불해주세요', date: '2022-03-03', 조회: 23 },
+  ]
 
-  const [toggle, setToggle] = useState(false);
-  const [int, setInt] = useState(0);
+  const [toggle, setToggle] = useState(false)
+  const [int, setInt] = useState(0)
 
   const onClickToggle = (index) => () => {
-    setToggle((prev) => !prev);
-    setInt(index);
-  };
+    setToggle((prev) => !prev)
+    setInt(index)
+  }
+
+  const [onDisplay, setdisplay] = useState(false)
+  const onVisible = () => {
+    setdisplay(true)
+  }
+
+  const onClickVisible = () => {
+    setdisplay(false)
+  }
 
   return (
     <S.Wrapper>
       <S.WrapperBox>
-        <div>
-          <S.WrapperTitle>고객센터</S.WrapperTitle>
-        </div>
         <S.WrapperTable>
-          <div>
-            <div>공지사항</div>
-          </div>
+          <S.TableSelect>
+            <S.WrapperTitle>고객센터</S.WrapperTitle>
+            <S.CommonIndex onClick={onVisible}>공지사항</S.CommonIndex>
+            <S.CommonIndex onClick={onClickVisible}>자주묻는질문</S.CommonIndex>
+          </S.TableSelect>
 
-          <S.TableBox>
-            <S.TableList style={{ backgroundColor: "#FAFAFA" }}>
-              <S.TableListRow>번호</S.TableListRow>
-              <S.TableListLongRow>제목</S.TableListLongRow>
-              <S.TableListRow>작성일</S.TableListRow>
-              <S.TableListRow>조회</S.TableListRow>
-            </S.TableList>
-            {noticeArr.map((el, index) => (
-              <div key={index}>
-                <S.TableList onClick={onClickToggle(index)}>
-                  <S.TableListRow>{el.index}</S.TableListRow>
-                  <S.TableListLongRow>{el.title}</S.TableListLongRow>
-                  <S.TableListRow>{el.date}</S.TableListRow>
-                  <S.TableListRow>{el.조회}</S.TableListRow>
-                </S.TableList>
-                {toggle && <NoticeItem el={el.index - 1} int={int} />}
-              </div>
-            ))}
-          </S.TableBox>
+          <S.ParentTable>
+            <S.TableBox onDisplay={onDisplay}>
+              <S.CommonsTitle>공지사항</S.CommonsTitle>
+              <S.TableList style={{ backgroundColor: '#FAFAFA' }}>
+                <S.TableListRow>번호</S.TableListRow>
+                <S.TableListLongRow>제목</S.TableListLongRow>
+                <S.TableListRow>작성일</S.TableListRow>
+                <S.TableListRow>조회</S.TableListRow>
+              </S.TableList>
+              {noticeArr.map((el, index) => (
+                <div key={index}>
+                  <S.TableList onClick={onClickToggle(index)}>
+                    <S.TableListRow>{el.index}</S.TableListRow>
+                    <S.TableListLongRow>{el.title}</S.TableListLongRow>
+                    <S.TableListRow>{el.date}</S.TableListRow>
+                    <S.TableListRow>{el.조회}</S.TableListRow>
+                  </S.TableList>
+                  {toggle && <NoticeItem el={el.index - 1} int={int} />}
+                </div>
+              ))}
+            </S.TableBox>
+            <S.RightTableBox onDisplay={onDisplay}>
+              <S.CommonsTitle>자주묻는질문</S.CommonsTitle>
+              <S.TableList style={{ backgroundColor: '#FAFAFA' }}>
+                <S.TableListRow>번호</S.TableListRow>
+                <S.TableListLongRow>제목</S.TableListLongRow>
+                <S.TableListRow>작성일</S.TableListRow>
+                <S.TableListRow>조회</S.TableListRow>
+              </S.TableList>
+              {QnaArr.map((el, index) => (
+                <div key={index}>
+                  <S.TableList onClick={onClickToggle(index)}>
+                    <S.TableListRow>{el.index}</S.TableListRow>
+                    <S.TableListLongRow>{el.title}</S.TableListLongRow>
+                    <S.TableListRow>{el.date}</S.TableListRow>
+                    <S.TableListRow>{el.조회}</S.TableListRow>
+                  </S.TableList>
+                  {toggle && <NoticeItem el={el.index - 1} int={int} />}
+                </div>
+              ))}
+            </S.RightTableBox>
+          </S.ParentTable>
         </S.WrapperTable>
       </S.WrapperBox>
     </S.Wrapper>
-  );
+  )
 }
--- src/components/units/market/detail/useditemDetail.container.tsx
+++ src/components/units/market/detail/useditemDetail.container.tsx
@@ -1,24 +1,19 @@
-import * as S from "./useditemDetail.styled";
-import { FiShare2, FiHeart, FiShoppingCart } from "react-icons/fi";
-import { BsFillEyeFill } from "react-icons/bs";
-import { useEffect } from "react";
-import { useState } from "react";
-import SellerInfo from "../../profile/fetchSeller/fetchSeller.container";
-import { useRef } from "react";
+import { useEffect } from 'react'
+import { useState } from 'react'
+import UseditemDetailPageUI from './useditemDetail.presenter'
 
 declare const window: typeof globalThis & {
-  //window 안에 카카오 넣어주는 것
-  Kakao: any;
-};
+  Kakao: any
+}
 
 export default function UseditemDetailPage() {
   //   const el = useRef();
-  const [isOpen, setIsOpen] = useState(false);
-  const [isHeart, setIsHeart] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const [isHeart, setIsHeart] = useState(false)
 
   const onClickOpen = () => {
-    setIsOpen((prev) => !prev);
-  };
+    setIsOpen((prev) => !prev)
+  }
 
   //   const CloseModal = ({ target }) => {
   //     console.log(!el.current.contains(target));
@@ -35,51 +30,43 @@ export default function UseditemDetailPage() {
   //   }, []);
 
   const onClickHeart = () => {
-    setIsHeart((prev) => !prev);
+    setIsHeart((prev) => !prev)
     setTimeout(() => {
-      setIsHeart((prev) => !prev);
-    }, 1000);
-  };
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-  };
+      setIsHeart((prev) => !prev)
+    }, 1000)
+  }
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://developers.kakao.com/sdk/js/kakao.js";
-    script.async = true;
+    const script = document.createElement('script')
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js'
+    script.async = true
 
-    document.body.appendChild(script);
+    document.body.appendChild(script)
 
     script.onload = () => {
-      createKakaoButton();
-    };
-  }, []);
+      createKakaoButton()
+    }
+  }, [])
 
   const createKakaoButton = () => {
     // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
     if (window.Kakao) {
-      const kakao = window.Kakao;
+      const kakao = window.Kakao
 
       // 중복 initialization 방지
       if (!kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init("a6737013adeb3b6bc159d974a287f791");
+        kakao.init('a6737013adeb3b6bc159d974a287f791')
       }
 
       kakao.Link.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
-        container: "#kakao-link-btn",
-        objectType: "feed",
+        container: '#kakao-link-btn',
+        objectType: 'feed',
         content: {
-          title: "타이틀",
-          description: "#리액트 #카카오 #공유버튼",
-          imageUrl: "http://localhost:3000/vercel.svg", // i.e. process.env.FETCH_URL + '/logo.png'
+          title: '타이틀',
+          description: '#리액트 #카카오 #공유버튼',
+          imageUrl: 'http://localhost:3000/vercel.svg', // i.e. process.env.FETCH_URL + '/logo.png'
           link: {
             mobileWebUrl: window.location.href,
             webUrl: window.location.href,
@@ -92,147 +79,31 @@ export default function UseditemDetailPage() {
         },
         buttons: [
           {
-            title: "웹으로 보기",
+            title: '웹으로 보기',
             link: {
               mobileWebUrl: window.location.href,
               webUrl: window.location.href,
             },
           },
           {
-            title: "앱으로 보기",
+            title: '앱으로 보기',
             link: {
               mobileWebUrl: window.location.href,
               webUrl: window.location.href,
             },
           },
         ],
-      });
+      })
     }
-  };
+  }
 
   return (
-    <S.Wrapper>
-      <S.WrapperBody>
-        <h2 style={{ textAlign: "center" }}>
-          디올 에센셜 선글라스 10A0 Dior Essential
-        </h2>
-        <S.WrapperTitle>
-          <S.TitleBox>
-            <S.TitleImg src="/sunglass.jpeg" />
-          </S.TitleBox>
-          <S.TitleBox>
-            <S.Titleprice>
-              <S.PriceStyleColor>380,000</S.PriceStyleColor>
-              <span>원</span>
-            </S.Titleprice>
-            <div style={{ display: "flex" }}>
-              <S.IconBox>
-                <FiHeart />0
-              </S.IconBox>
-              <S.IconBox>
-                <BsFillEyeFill /> 0
-              </S.IconBox>
-              <S.IconBox>
-                <FiShoppingCart />
-                장바구니
-              </S.IconBox>
-              <S.IconBox className="kakao-share-button">
-                {/* <FiShare2 style={{fontSize:28, marginRight:"5px"}}/>공유 */}
-                <S.ShareButton id="kakao-link-btn">
-                  <FiShare2 />
-                  공유하기
-                </S.ShareButton>
-              </S.IconBox>
-            </div>
-            <div style={{ display: "flex" }}>
-              <S.WrapperBodyUl>
-                <S.WrapperLiStyle>상품상태</S.WrapperLiStyle>
-                <S.WrapperLiStyle>브랜드</S.WrapperLiStyle>
-                <S.WrapperLiStyle>상품종류</S.WrapperLiStyle>
-                <S.WrapperLiStyle>구입년도</S.WrapperLiStyle>
-              </S.WrapperBodyUl>
-              <S.WrapperBodyUl>
-                <S.WrapperLiStyle>중고</S.WrapperLiStyle>
-                <S.WrapperLiStyle>디올</S.WrapperLiStyle>
-                <S.WrapperLiStyle>상의</S.WrapperLiStyle>
-                <S.WrapperLiStyle>2021.10</S.WrapperLiStyle>
-              </S.WrapperBodyUl>
-            </div>
-
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <S.ButtonStyle onClick={onClickHeart}>
-                <S.FiHeartIcon />
-                찜하기
-              </S.ButtonStyle>
-              <S.ButtonStyle>문의하기</S.ButtonStyle>
-              <S.ButtonBlackStyle>구매하기</S.ButtonBlackStyle>
-            </div>
-            <div>
-              <h4
-                style={{
-                  marginBottom: "5px",
-                }}
-              >
-                판매자 정보
-              </h4>
-              <S.SellerBox onClick={onClickOpen}>
-                <S.SellerProfile></S.SellerProfile>
-                <div>
-                  <p>유저닉네임</p>
-                  <p>판매 상품 0 거래 후기 0</p>
-                </div>
-              </S.SellerBox>
-              <h4>판매자의 다른 상품도 만나보세요</h4>
-              <S.SellerBox>
-                <S.SellerProduct>550,000원</S.SellerProduct>
-                <S.SellerProduct>550,000원</S.SellerProduct>
-                <S.SellerProduct>550,000원</S.SellerProduct>
-              </S.SellerBox>
-            </div>
-          </S.TitleBox>
-        </S.WrapperTitle>
-        <S.SelectBox></S.SelectBox>
-        <S.ContentsBox>
-          디올 에센셜 선글라스입니다. <br />
-          공식 온라인 구매 상품으로
-          <br />
-          보증서 포함된 완벽한 새상품 + 정품입니다.
-          <br />
-          남녀 모두 사용하시기 좋은 심플한 디자인입니다.
-          <br />
-          <S.ContentsImg src="/sunglass.jpeg" />
-          <br />
-          <S.ContentsImg src="/sunglass02.jpeg" />
-          <br />
-          <S.ContentsImg src="/sunglass03.jpeg" />
-          <br />
-          <S.ContentsImg src="/sunglass04.jpeg" />
-          <br />
-        </S.ContentsBox>
-        <div>
-          <S.RelativeTitle>관련상품</S.RelativeTitle>
-          <S.SliderContainer {...settings}>
-            <S.SliderBox></S.SliderBox>
-            <S.SliderBox></S.SliderBox>
-            <S.SliderBox></S.SliderBox>
-            <S.SliderBox></S.SliderBox>
-            <S.SliderBox></S.SliderBox>
-            <S.SliderBox></S.SliderBox>
-          </S.SliderContainer>
-        </div>
-      </S.WrapperBody>
-      <S.WrapperSellerInfo isOpen={isOpen}>
-        <SellerInfo isOpen={isOpen} setIsOpen={setIsOpen} />
-      </S.WrapperSellerInfo>
-      <S.HeartBox isHeart={isHeart}>
-        <S.HeartOnbox>
-          <S.HeartFill fontSize={52} />
-          <p>
-            위시리스트에 <br />
-            추가되었습니다.
-          </p>
-        </S.HeartOnbox>
-      </S.HeartBox>
-    </S.Wrapper>
-  );
+    <UseditemDetailPageUI
+      isHeart={isHeart}
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      onClickHeart={onClickHeart}
+      onClickOpen={onClickOpen}
+    />
+  )
 }
