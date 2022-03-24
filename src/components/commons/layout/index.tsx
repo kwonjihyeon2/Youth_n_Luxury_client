0367ceb090bfc32bd37fbe99325f5c59c393b219
RumbleBi <olzlel2000@gmail.com>
2022-03-24 21:02:22 +0900

    Feat: 레이아웃 푸터 작업 완료, 어드민 페이지는 다음 이슈에서 작업할 것

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,24 +1,26 @@
-import LayoutSidebar from "./sidebar";
-import styled from "@emotion/styled";
-import { ReactChild } from "react";
-import { useRouter } from "next/router";
+import LayoutSidebar from './sidebar'
+import styled from '@emotion/styled'
+import { ReactChild } from 'react'
+import { useRouter } from 'next/router'
+import LayoutFooter from './footer'
 
 interface ILayoutProps {
-  children: ReactChild;
+  children: ReactChild
 }
 
-const LayoutBody = styled.div``;
+const LayoutBody = styled.div``
 
 export default function Layout(props: ILayoutProps) {
-  const router = useRouter();
-  const HIDDEN_SIDEBAR = ["/market/new"];
-  const isHiddenSidebar = HIDDEN_SIDEBAR.includes(router.asPath);
+  const router = useRouter()
+  const HIDDEN_SIDEBAR = ['/market/new']
+  const isHiddenSidebar = HIDDEN_SIDEBAR.includes(router.asPath)
   return (
     <>
       <LayoutBody>
         {!isHiddenSidebar && <LayoutSidebar />}
         {props.children}
       </LayoutBody>
+      <LayoutFooter />
     </>
-  );
+  )
 }
---
+++ src/components/units/admin/main/AdminMain.presenter.tsx
@@ -0,0 +1,9 @@
+import * as S from './AdminMain.styles'
+
+export default function AdminMainUI() {
+  return (
+    <S.Position>
+      <S.Wrapper></S.Wrapper>
+    </S.Position>
+  )
+}
