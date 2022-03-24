14720b0342a667db6b27055a8abf0eae079b931d
Gyubin <hgb072082@gmail.com>
2022-03-24 11:23:48 +0900

    Feat: 에러수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Main from '../src/components/market/main/Main.container'
-import SideBar from '../src/components/units/market/myPage/sideBar/SideBar.container'
 
 export default function Home() {
   return <Main />
