d92e9bb54647d60af2dbea823de333577b698532
RumbleBi <olzlel2000@gmail.com>
2022-03-30 21:35:56 +0900

    Style: 어드민페이지 기능구현중이었음

--- src/components/units/admin/main/AdminMain.container.tsx
+++ src/components/units/admin/main/AdminMain.container.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
 import AdminMainUI from './AdminMain.presenter'
 
 export default function AdminMain() {
+  const router = useRouter()
+  const { data, refetch } = useQuery()
   return <AdminMainUI />
 }
